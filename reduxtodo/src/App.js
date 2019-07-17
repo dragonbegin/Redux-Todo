61b026951d7a2132534885c64807541edfea72bb
dragonbegin <juan.reyes.mba@gmail.com>
2019-07-16 22:57:16 -0500

    remidial code solution for reference

--- reduxtodo/src/App.js
+++ reduxtodo/src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 import { connect } from 'react-redux';
+import { addTodo } from './actions';
 
-function App(props) {
-  return (
-    <div className="App">
-      <h1> To Do List </h1>
-      {
-        props.todos.map((item) => {
-          return (
-            <div>
-              <p>
-              {
-                item.value
-              }
-              </p>
-            </div>
-          )
-        })
-      }
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoText: ''
+    };
+  }
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addTodo = () => {
+    const { todoText } = this.state;
+    const newTodo = {
+      id: this.props.todos.length + 1,
+      completed: false,
+      text: todoText
+    };
+    this.props.addTodo(newTodo);
+    this.setState({ todoText: '' });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ToDo List {this.props.name}</h1>
+        </header>
+        <TodoForm
+          handleInputChange={this.handleInputChange}
+          todoText={this.state.todoText}
+          addTodo={this.addTodo}
+        />
+        <div className="App-intro">
+          <Todos todos={this.props.todos} />
+        </div>
+      </div>
+    );
   }
 }
 
-export default connect(mapStateToProps)(App); 
+const mapStateToProps = state => {
+  return {
+    todos: state.todos,
+    name: state.name
+  };
+};
+
+export default connect(mapStateToProps, { addTodo })(App);
--- reduxtodo/src/index.js
+++ reduxtodo/src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// import { importSpecifier } from '@babel/types';
-import reducer  from './reducer'
 
-const store = createStore(reducer) ;
+import './index.css';
+import App from './App';
+import { todosReducer } from './reducers';
 
+const store = createStore(todosReducer);
 
- const app = (
-   <Provider store = {store} >
-      <App />
-   </Provider>
-
- )
-
-ReactDOM.render( app, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
